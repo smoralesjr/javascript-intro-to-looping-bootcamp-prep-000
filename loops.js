cd47a49efadb7af253a1d3906659cc65d89f76d4
Samuel Morales Jr <smoralesjr@gmail.com>
2018-09-28 21:19:54 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,8 +2,8 @@ const forLoop = function(array){
   for (let count = 0, count < 25, count++){
     if (count === 1){
       console.log("I am ${count} strange loop.");
-    }; else {
+    } else {
       console.log("I am ${count} strange loops.");
-    }
+    };
   }
 }
